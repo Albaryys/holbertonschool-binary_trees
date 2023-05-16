93a3e7053e6a09f570cae17a37c893d7b15c17d4
GaetanLecuyer <[200~6229@holbertonstudents.com~>
2023-05-16 14:12:15 +0200

    modification des declaration variable

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -1,5 +1,7 @@
 #include "binary_trees.h"
 
+#include "binary_trees.h"
+
 /**
  * binary_tree_sibling - Find the sibling of a node
  * @node: Pointer to the node to find the sibling
@@ -8,10 +10,12 @@
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
+	binary_tree_t *parent;
+
 	if (node == NULL || node->parent == NULL)
 		return (NULL);
 
-	binary_tree_t *parent = node->parent;
+	parent = node->parent;
 
 	if (parent->left == node)
 		return (parent->right);
