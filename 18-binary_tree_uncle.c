59fc80e512db9b1f173811e456abc5da8d49ef8c
GaetanLecuyer <[200~6229@holbertonstudents.com~>
2023-05-16 14:17:35 +0200

    modification des declaration variable

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -1,10 +1,10 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_uncle - Find the uncle of a node
- * @node: Pointer to the node to find the uncle
+ * binary_tree_sibling - Find the sibling of a node
+ * @node: Pointer to the node to find the sibling
  *
- * Return: Pointer to the uncle node, or NULL if not found
+ * Return: Pointer to the sibling node, or NULL if not found
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
@@ -14,8 +14,8 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
 		return (NULL);
 
-	binary_tree_t *parent = node->parent;
-	binary_tree_t *grandparent = parent->parent;
+	parent = node->parent;
+	grandparent = parent->parent;
 
 	if (grandparent->left == parent)
 		return (grandparent->right);
