77a923ff3c8cbb6326149e3b474b0ba7d830a129
GaetanLecuyer <[200~6229@holbertonstudents.com~>
2023-05-16 13:37:06 +0200

    moication declation variable

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,21 +1,15 @@
 #include "binary_trees.h"
 
+int binary_tree_balance(const binary_tree_t *tree);
 int binary_tree_height(const binary_tree_t *tree);
 int max(int a, int b);
 
-/**
- * binary_tree_balance - Measure the balance factor of a binary tree
- * @tree: Pointer to the root node of the tree
- *
- * Return: The balance factor of the tree
- */
 int binary_tree_balance(const binary_tree_t *tree)
 {
+    int left_height, right_height;
+
     if (tree == NULL)
         return (0);
-
-    int left_height;
-    int right_height;
 
     left_height = binary_tree_height(tree->left);
     right_height = binary_tree_height(tree->right);
@@ -23,19 +17,12 @@ int binary_tree_balance(const binary_tree_t *tree)
     return (left_height - right_height);
 }
 
-/**
- * binary_tree_height - Measure the height of a binary tree
- * @tree: Pointer to the root node of the tree
- *
- * Return: The height of the tree
- */
 int binary_tree_height(const binary_tree_t *tree)
 {
+    int left_height, right_height;
+
     if (tree == NULL)
         return (-1);
-
-    int left_height;
-    int right_height;
 
     left_height = binary_tree_height(tree->left);
     right_height = binary_tree_height(tree->right);
@@ -43,13 +30,6 @@ int binary_tree_height(const binary_tree_t *tree)
     return (1 + max(left_height, right_height));
 }
 
-/**
- * max - Find the maximum of two numbers
- * @a: First number
- * @b: Second number
- *
- * Return: The maximum of the two numbers
- */
 int max(int a, int b)
 {
     return (a > b ? a : b);
