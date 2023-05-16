a58657dc5d4471b904f14fffaeb00c7310deaded
GaetanLecuyer <[200~6229@holbertonstudents.com~>
2023-05-16 14:05:12 +0200

    modification des declaration de function

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,5 +1,4 @@
 #include "binary_trees.h"
-#include "binary_trees.h"
 
 int binary_tree_height(const binary_tree_t *tree);
 int max(int a, int b);
@@ -14,10 +13,8 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	int left_height, right_height;
-
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
+	int left_height = binary_tree_height(tree->left);
+	int right_height = binary_tree_height(tree->right);
 
 	if (left_height == right_height)
 	{
@@ -39,13 +36,11 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
  */
 int binary_tree_height(const binary_tree_t *tree)
 {
-	int left_height, right_height;
-
 	if (tree == NULL)
 		return (-1);
 
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
+	int left_height = binary_tree_height(tree->left);
+	int right_height = binary_tree_height(tree->right);
 
 	if (left_height > right_height)
 		return (left_height + 1);
